3adc1a4beb51b86137ec8cc43fb7d7315f6253dc
khave <kristian.have@hotmail.com>
2019-11-18 15:12:07 +0100

    Added comments and fixed imports

--- src/java.com.workshop/BuildingSystem.java
+++ src/java.com.workshop/BuildingSystem.java
@@ -3,6 +3,9 @@ package java.com.workshop;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The type Building system.
+ */
 public class BuildingSystem {
 
     private List<Building> buildings;
@@ -14,10 +17,20 @@ public class BuildingSystem {
         buildings = new ArrayList<>();
     }
 
+    /**
+     * Add building.
+     *
+     * @param building the building
+     */
     public void addBuilding(Building building){
         buildings.add(building);
     }
 
+    /**
+     * Remove building.
+     *
+     * @param building the building
+     */
     public void removeBuilding(Building building){
         buildings.remove(building);
     }
