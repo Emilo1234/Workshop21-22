1cd286d93295b69ab01f01929055ff26684d6b66
khave <kristian.have@hotmail.com>
2019-11-18 15:48:14 +0100

    Added Building framework

--- src/java.com.workshop/Building.java
+++ src/java.com.workshop/Building.java
@@ -1,8 +1,41 @@
 package java.com.workshop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Building {
 
-    public Building(){
+    private String name;
+    private List<Sensor> sensors;
+    private List<Actuator> actuators;
 
+    public Building(String name){
+        this.name = name;
+        this.sensors = new ArrayList<>();
+        this.actuators = new ArrayList<>();
+    }
+
+    public void addSensor(Sensor sensor){
+        sensors.add(sensor);
+    }
+
+    public void addActuator(Actuator actuator){
+        actuators.add(actuator);
+    }
+
+    public void removeSensor(Sensor sensor){
+        sensors.remove(sensor);
+    }
+
+    public void removeActuator(Actuator actuator){
+        actuators.remove(actuator);
+    }
+
+    public List<Actuator> getActuators() {
+        return actuators;
+    }
+
+    public List<Sensor> getSensors() {
+        return sensors;
     }
 }
